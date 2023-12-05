e04e4e44f13f7be14c67a3c93a283aba055a9885
adepge <adepge@gmail.com>
2023-12-05 12:00:59 +0000

    Lesson #12 onwards

---
+++ my-react-practice-app/src/Home.tsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+import BlogList from "./BlogList";
+
+const Home = () => {
+    const[blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ]);
+    
+    const handleDelete = (id: number) => {
+        const newBlogs = blogs.filter((blog) => blog.id !== id);
+        setBlogs(newBlogs);
+    }
+
+    useEffect(() =>{
+        console.log('use effect ran')
+        console.log(blogs)
+    });
+
+    return ( 
+        <div className="home">
+            <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
+            {/* <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's blogs"/> */}
+        </div>
+    );
+}
+ 
+export default Home;
